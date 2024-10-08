3cfa643842d2f952835a34d3d5b17a8e44638d76
Marco Faustinelli <marco.faustinelli@edenred.com>
2024-10-08 04:23:07 +0200

    eventualprogramming (crockford book) - ready to study

---
+++ eventualprogramming-crockford/babel.config.js
@@ -0,0 +1,15 @@
+export default (api) => {
+  // Cache configuration is a required option
+  api.cache(false);
+
+  const presets = [
+    [
+      "@babel/preset-env", 
+      { 
+        useBuiltIns: false
+      }
+    ]
+  ];
+
+  return { presets };
+};
